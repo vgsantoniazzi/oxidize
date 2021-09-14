b2f353613f04cd2e250463ebd09d25fc8f845446
Victor Antoniazzi <vgsantoniazzi@gmail.com>
2021-09-14 12:03:51 -0300

    Add keyboard support

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
+#![feature(abi_x86_interrupt)]
 #![no_std]
 #![no_main]
 
 use core::panic::PanicInfo;
 
+mod gdt;
 mod macros;
 mod screen;
+mod keyboard;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    gdt::init();
+    keyboard::initialize();
+    unsafe { keyboard::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
     println!("Welcome to Oxidize!!");
-    panic!("Error!");
+    loop { x86_64::instructions::hlt(); }
 }
 
 // This function is called on panic.
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop {}
+    loop { x86_64::instructions::hlt(); }
 }
